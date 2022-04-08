d98ed03bd1490f44ad33f8fed2bc57afbdc0e4da
henriquesan25 <99773818+henriquesan25@users.noreply.github.com>
2022-04-08 20:57:37 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const{createServer} = require("http")
+let server = createServer ((request,response)=>{
+response.writeHead(200,{"Content-type":"text/html"});
+response.write('<h1> Hello Word </h1>');
+response.end();
+});
+server.listen(8000);
+console.log("Sevidor Iniciado !!");
