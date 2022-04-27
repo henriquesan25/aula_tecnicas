3968ba0f883f991c5df01d547a3ebace3de7e800
henriquesan25 <99773818+henriquesan25@users.noreply.github.com>
2022-04-26 21:22:45 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
-const{createServer} = require("http")
-let server = createServer ((request,response)=>{
-response.writeHead(200,{"Content-type":"text/html"});
-response.write('<h1> Hello Word </h1>');
-response.end();
+/*const{createServer,request}=require('http')
+let server = createServer((request,Response)=>{
+Response.writeHead(200,{"Content_type": "text/html"});
+Response.write('<h1>Ola Mundo</h1><p>Projeto</p>');
+Response.end();
 });
 server.listen(8000);
-console.log("Sevidor Iniciado !!");
+console.log('Projeto Iniciado');
 
-console.log("Caue");
-console.log("Nova atualização Cauê")
-console.log("henrique");
+let express = require('express');
+let app = express();
+
+app.get('/', function(req,res){
+    res.send('ROTA GET')
+});
+
+app.get('/rota1',function(req,res){
+    res.send('rota 1');
+});
+
+app.listen(3000,function(){
+    console.log('Projeto iniciado na porta 3000');
+});*/
+
+app.route('/book')
+    .get(function(req, res) {
+        res.send('get a random book');
+    });
+
+    .post(function(req, res){
+        res.send('add a book');
+    });
